4287616d9224ec1118554c5ba49e71a7a17d02c3
natali2205 <NataliaKovalchuk37@gmail.com>
2018-05-14 15:03:05 +0300

    Task3

--- src/Task3/MarketHtmlPresenter.php
+++ src/Task3/MarketHtmlPresenter.php
@@ -1,1 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Cryptocurrency\Task3;
+
+use Cryptocurrency\Task1\CoinMarket;
+
+class MarketHtmlPresenter
+{
+    public function present(CoinMarket $market): string
+    {
+
+        {
+            $html = '';
+            foreach ($market->getCurrencies() as $coin) {
+                $html .= '<img src="' . $coin->getLogoUrl() . '"><br>';
+                $html .= $coin->getName() . ': ';
+                $html .= $coin->getDailyPrice() . '<br>';
+
+            }
             return $html;
+        }
+    }
+}
